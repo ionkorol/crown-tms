42e06c2c7a148e957382be33e3c7e728923555b8
Ion Sobol <ionsobol@gmail.com>
2021-06-02 13:58:00 -0400

    First

--- pages/api/pdf.ts
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import { NextApiRequest, NextApiResponse } from "next";
-import { Document, Page, View } from '@react-pdf/renderer'
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).send({ name: "John Doe" });
-};
