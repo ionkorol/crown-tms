2c519ab604edd200a00145aecb8b339cbf74a776
Ion Sobol <ionsobol@gmail.com>
2021-07-14 17:50:53 -0400

    New Version

--- pages/test.tsx
+++ pages/test.tsx
@@ -2,14 +2,15 @@ import { getLoads } from "lib/api/Loads";
 import { GetServerSideProps } from "next";
 import React from "react";
 
-const test = () => {
+const test = (props) => {
+  console.log(props);
   return <div></div>;
 };
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   return {
     props: {
-      data: getLoads("100"),
+      data: await getLoads("100"),
     },
   };
 };
